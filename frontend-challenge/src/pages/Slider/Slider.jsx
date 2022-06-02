cd5ce61dbe12631a1c25c6f21394464502d47e1e
Reid Schroder <reidschroder2@gmail.com>
2022-06-02 11:55:31 -0400

    unsuccessful slider attempt

--- frontend-challenge/src/pages/Slider/Slider.jsx
+++ frontend-challenge/src/pages/Slider/Slider.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import './Slider.scss';
 
+
+
 const Slider = () => {
     return (
        // creating a container for the slider. 32 slides in total for the final product
-        <div class="container">
-          <div class="slide">
+        <div className="container">
+
+          <div className="slide">
               <img src="../../assets/images/AFC/AFC_north/joe_burrow.jpg" alt="Joe Burrow" />
-            <h2>Joe Burrow</h2>
-            <h4>Cincinnati Bengals</h4>
+              <img src="../../assets/images/AFC/AFC_north/baker_mayfield.jpg" alt="Baker Mayfield" />
+              <img src="../../assets/images/AFC/AFC_north/lamar_jackson.jpg" alt="Lamar Jackson" />
+              <img src="../../assets/images/AFC/AFC_north/mitch_trubisky.jpg" alt="Mitch Trubisky" />
           </div>
-          <div class="slide">
-            <img src="../../assets/images/AFC/AFC_north/baker_mayfield.jpg" alt="Baker Mayfield" />
+          {/* <div className="slide">
+            
           <h2>Baker Mayfield</h2>
           <h4>Cleveland Browns</h4>
         </div>
-        <div class="slide">
-            <img src="../../assets/images/AFC/AFC_north/lamar_jackson.jpg" alt="Lamar Jackson" />
+        <div className="slide">
+            
           <h2>Lamar Jackson</h2>
           <h4>Baltimore Ravens</h4>
         </div>
-        <div class="slide">
-            <img src="../../assets/images/AFC/AFC_north/mitch_trubisky.jpg" alt="Mitch Trubisky" />
+        <div className="slide">
+            
           <h2>Mitch Trubisky</h2>
           <h4>Pittsburgh Steelers</h4>
+        </div> */}
+        <div className='last'></div>
+        <div className='next'></div>
         </div>
-        </div>
+      
     );
 }
 
